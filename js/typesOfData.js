50cf7b71b057ab28d2347d683e52a4975ba40e6e
TehnikumCodingFrontend <tehnikum.coding.frontend@gmail.com>
2023-08-28 00:31:11 +0500

    first commit

---
+++ js/typesOfData.js
@@ -0,0 +1,122 @@
+export const movieObj = {
+  Title: "Moneyball",
+  Year: "2011",
+  Rated: "PG-13",
+  Released: "23 Sep 2011",
+  Runtime: "133 min",
+  Genre: "Biography, Drama, Sport",
+  Director: "Bennett Miller",
+  Writer: "Steven Zaillian, Aaron Sorkin, Stan Chervin",
+  Actors: "Brad Pitt, Robin Wright, Jonah Hill",
+  Plot: "Oakland A's general manager Billy Beane's successful attempt to assemble a baseball team on a lean budget by employing computer-generated analysis to acquire new players.",
+  Language: "English",
+  Country: "United States",
+  Awards: "Nominated for 6 Oscars. 29 wins & 82 nominations total",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMjAxOTU3Mzc1M15BMl5BanBnXkFtZTcwMzk1ODUzNg@@._V1_SX300.jpg",
+  Ratings: [
+    {
+      Source: "Internet Movie Database",
+      Value: "7.6/10",
+    },
+    {
+      Source: "Rotten Tomatoes",
+      Value: "94%",
+    },
+    {
+      Source: "Metacritic",
+      Value: "87/100",
+    },
+  ],
+  Metascore: "87",
+  imdbRating: "7.6",
+  imdbVotes: "445,384",
+  imdbID: "tt1210166",
+  Type: "movie",
+  DVD: "03 Sep 2013",
+  BoxOffice: "$75,605,492",
+  Production: "N/A",
+  Website: "N/A",
+  Response: "True",
+};
+
+export const searchResult = {
+  Search: [
+    {
+      Title: "Tenet",
+      Year: "2020",
+      imdbID: "tt6723592",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMzU3YWYwNTQtZTdiMC00NjY5LTlmMTMtZDFlYTEyODBjMTk5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Tenet: 10 min Behind the Scenes",
+      Year: "2020",
+      imdbID: "tt13061794",
+      Type: "movie",
+      Poster: "N/A",
+    },
+    {
+      Title: "Looking at the World in a New Way: The Making of Tenet",
+      Year: "2020",
+      imdbID: "tt13920556",
+      Type: "movie",
+      Poster: "N/A",
+    },
+    {
+      Title: "Tenet Noctis",
+      Year: "2019",
+      imdbID: "tt6141830",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYmUwMTI5MjYtMjFiMi00MGRhLWFjZDMtYjBlZjQ2YmFiMWI3L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMzM5NzYyNjc@._V1_SX300.jpg",
+    },
+    {
+      Title: "Roma te tenet et amoR",
+      Year: "2011",
+      imdbID: "tt2069901",
+      Type: "movie",
+      Poster: "N/A",
+    },
+    {
+      Title: "The Tenet",
+      Year: "2018",
+      imdbID: "tt3626142",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMmFiZjRkYjEtMTgxMS00ZjZlLTkyNjctNGVhMTg1ODQ2MjQxXkEyXkFqcGdeQXVyOTQxODA4Nw@@._V1_SX300.jpg",
+    },
+    {
+      Title: "Dark Tenet",
+      Year: "2014",
+      imdbID: "tt2243233",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNzdlNmIzNjMtYzMyNS00NjkwLWJkNjAtNjhlNzIyZTIyZDUwXkEyXkFqcGdeQXVyNDk3OTU1ODc@._V1_SX300.jpg",
+    },
+    {
+      Title: "40 - Tenet (2020)",
+      Year: "2020",
+      imdbID: "tt23870124",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMWNmYTYxNWUtZTA1Yy00N2Q3LWExNzMtNWMwYzRkYWZiMTNiXkEyXkFqcGdeQXVyMTIwODg3NTEy._V1_SX300.jpg",
+    },
+    {
+      Title: "Tenting for Tenet",
+      Year: "2021",
+      imdbID: "tt13612024",
+      Type: "movie",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMGU0YTU1NmEtZWFkYy00NDFjLWIwYzUtYzVhODBlM2E0YzRiXkEyXkFqcGdeQXVyNDcwMTEwNjY@._V1_SX300.jpg",
+    },
+  ],
+  totalResults: "9",
+  Response: "True",
+};
+
+const errorObj = {
+  Response: "False",
+  Error: "Movie not found!",
+};
